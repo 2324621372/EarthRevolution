21cbb3040bc78e31e6053fe5b1a19889937e7b64
LAPTOP-CH8JD2NE\郑某人 <2324621372@qq.com>
2023-05-20 23:37:35 +0800

    资源载入

---
+++ Assets/Script/GameManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum State
+{
+    StateRevoluation = 0,
+    StateRotation = 1,
+    StateDemarcationLine = 2,
+    StateFiveBelts = 3
+}
+public class GameManager : MonoBehaviour
+{
+  
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
